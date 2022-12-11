8aa0e8192e2ed385ebc8d12c7cb58513b69781d0
sidivalfn <33158182+sidivalfn@users.noreply.github.com>
2022-12-10 22:23:21 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("12345678", "Nokia", "zzzz1234", 50);
+Iphone iphone = new Iphone("012345678", "Iphone", "4321wwww", 60);
+
+nokia.InstalarAplicativo("Facebook");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+iphone.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
